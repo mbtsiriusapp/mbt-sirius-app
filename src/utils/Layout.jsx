79a68e4deb1acfb527873a45b3f055d390ba4cee
Vignesh Mariappan <vigneshmv2312@gmail.com>
2024-09-24 10:09:03 +0530

    safari overflow issue due to toolbar css fix

--- src/utils/Layout.jsx
+++ src/utils/Layout.jsx
@@ -3,11 +3,12 @@ import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import { Divider } from '@nextui-org/divider';
 import { VideoProvider } from './VideoListProvider';
+import { isMobile } from 'react-device-detect';
 
 const Layout = () => {
   return (
     <VideoProvider>
-      <div className='App'>
+      <div className='App' style={{ '--appHeight': isMobile ? '100dvh': '100vh' }} >
         <Header />
         <Divider />
         <Outlet />
